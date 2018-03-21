d6f90fe27f3d6175bc857f7a6d9d3d586a820f89
Jonata William <jonatawilliam@gmail.com>
2018-03-21 20:18:54 -0300

    Adicionado validações no Services de Cliente

--- src/services/ClienteService.java
+++ src/services/ClienteService.java
@@ -3,9 +3,17 @@ package services;
 import model.Cliente;
 
 public class ClienteService {
-    public boolean save(Cliente cliente) {
-        if (cliente.getName().length() <)
-
+    public Collection<String> isValid(Cliente cliente) {
+        ArrayList<String> erros = new ArrayList<>();
+        if (cliente.getName().length() < 5) {
+            erros.add("Nome menor que 5 caracteres.");
+        }
+        if (cliente.getPais() == null || cliente.getPais() == "") {
+            erros.add("País não pode ficar em branco.");
+        }
+        return erros;
+    }
+    public void save(Cliente cliente) {
         if (cliente.getIdade() < 18) {
             cliente.setLimiteCredito(100);
         } else if (cliente.getIdade() <= 35) {
